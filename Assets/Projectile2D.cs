37c5895b44ae2a07e9f138bd89a7b1a4d9791265
Tanit4567 <johnbatan09@gmail.com>
2025-05-02 04:31:36 +0700

    Code Projectile2D

---
+++ Assets/Projectile2D.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class Projectile2D : MonoBehaviour
+{
+    public Transform shootPoint;
+
+    public GameObject target;
+
+    public Rigidbody2D bulletPrefab;
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+
+    {
+
+        Vector2 distance = target - origin;
+
+        float velocityX = distance.x / time;
+
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+
+        return new Vector2(velocityX, velocityY);
+
+    }//
+
+    void Update()
+
+    {
+
+        if (Input.GetMouseButtonDown(0))
+
+        {
+
+            // ???????????????????? ???????????????? 2D
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.magenta, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+
+            {
+
+                // ???? Target ????????????????
+
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+
+                // ?????????????????????????
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+
+                // ???????????????
+
+                Rigidbody2D firedBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+
+                // ????????????????????
+
+                firedBullet.linearVelocity = projectileVelocity;
+
+            }//hit.collider
+
+        }//GetMouseButtonDown
+
+    }// Update
+
+}
