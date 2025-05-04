d959237ba977b5a0e60a58f211cff037c63a8aa1
Tanit4567 <johnbatan09@gmail.com>
2025-05-04 22:12:36 +0700

    Save

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int nScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
